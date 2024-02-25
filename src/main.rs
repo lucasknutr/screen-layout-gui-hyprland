b7c7784eb931da2c4896d6c4c68749cfd144cdd8
Lucas <lucasclaudinocc@gmail.com>
2024-02-25 10:53:59 -0300

    Adding eframe crate and basic ScreenData struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    // implement logic to eframe
+}
+
+struct ScreenData {
+    width: i32,
+    height: i32,
+    refresh_rate: i32,
+    position: i32,
 }
